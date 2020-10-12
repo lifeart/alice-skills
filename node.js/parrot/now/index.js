736d2f37761c39b89fedff45db693b413a357995
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-10-12 23:50:40 +0300

    add bash parsing

--- node.js/parrot/now/index.js
+++ node.js/parrot/now/index.js
@@ -1,28 +1,43 @@
 // Для асинхронной работы используется пакет micro.
 const { json } = require('micro');
+const fetch = require('node-fetch');
+const { parse } = require('node-html-parser');
 
 // Запуск асинхронного сервиса.
 module.exports = async (req, res) => {
 
-    // Из запроса извлекаются свойства request, session и version.
-    const { request, session, version } = await json(req);
+    try {
+        const { request, session, version } = await json(req);
+        const escapeWords = ['спасибо', 'хватит', 'выход'];
+        const isFinish = escapeWords.some((word) => request.original_utterance.includes(word));
 
-    // В тело ответа вставляются свойства version и session из запроса.
-    // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
-    res.end(JSON.stringify(
-        {
-            version,
-            session,
-            response: {
-                // В свойстве response.text возвращается исходная реплика пользователя.
-                // Если навык был активирован без дополнительной команды,
-                // пользователю нужно сказать "Hello!".
-                text: request.original_utterance || 'Hello!',
-
-                // Свойство response.end_session возвращается со значением false,
-                // чтобы диалог не завершался.
-                end_session: false,
-            },
+        if (isFinish) {
+            return res.end(JSON.stringify(
+                {
+                    version,
+                    session,
+                    response: {
+                        text: 'хорошего дня!',
+                        end_session: true
+                    }
+                }));
         }
-    ));
+
+        const response = await fetch('https://bash.im/random');
+        const body = await response.text();
+        const root = parse(body);
+
+        res.end(JSON.stringify(
+            {
+                version,
+                session,
+                response: {
+                    text: root.querySelector('.quote__body').innerText,
+                    end_session: false,
+                },
+            }
+        ));
+    } catch {
+        res.end('HELLO');
+    }
 };
